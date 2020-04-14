21f6a989a7a16a48a392e9198d7d189a819fde53
Rahime Tayfun <suheyladurna@gmail.com>
2020-04-14 16:40:06 -0500

    done

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,9 +12,11 @@ function Projects() {
     <div className="projects-container" id="projects">
         <h1>Projects</h1>
         <div className="project">
-          <img src={volevent} alt="volunteer" className="project-image"/>
+          <div className="project-image">
+            <img src={volevent} alt="volunteer" id="image"/>
+          </div>
           <div className="project-info">
-            <p>Volevent, personal project, is a fully responsive website where organizations can share their events in need of volunteers and volunteers can check upcoming events and attend them.
+            <p><strong>Volevent</strong>, personal project, is a fully responsive website where organizations can share their events in need of volunteers and volunteers can check upcoming events and attend them.
             <p><strong>Technologies:</strong> React, Redux, Node, Express, PostgreSQL, Massive, Sass, Bcrypt.js, Cloudinary, Sendgrid</p>
             <div className="external-links">
             <a href="https://github.com/rahimestayfun/dallas-volunteer-app" target="_blank" rel="noopener noreferrer"><Icon icon={githubIcon} width="45"/></a>
@@ -26,9 +28,11 @@ function Projects() {
         <hr/>
 
         <div className="project">
-          <img src={giftAway}  alt="giftaway" className="project-image"/>
+          <div className="project-image">
+            <img src={giftAway}  alt="giftaway" id="image"/>
+          </div>
           <div className="project-info">
-            <p>Gift Away, group project, is a fully responsive donations website where anyone can give away his/her unused or unwanted stuff at home
+            <p><strong>Gift Away</strong>, group project, is a fully responsive donations website where anyone can give away his/her unused or unwanted stuff at home
             <p><strong>Technologies:</strong> React, Redux, Node, Express, PostgreSQL, Massive, Sass, Bcrypt.js, Cloudinary,Bootstrap,Socket.io</p>
             <div className="external-links">
             <a href="https://github.com/wdl28-group-project/donationizer" target="_blank" rel="noopener noreferrer"><Icon icon={githubIcon} width="45"/></a>
@@ -40,9 +44,11 @@ function Projects() {
         <hr/>
 
         <div className="project">
-          <img src={storepo} alt="story-blog" className="project-image"/>
+          <div className="project-image">
+            <img src={storepo} alt="story-blog" id="image"/>
+          </div>
           <div className="project-info">
-            <p>Storepo is a blog site that anyone can share their first day experience at work with everyone.
+            <p><strong>Storepo</strong> is a blog site that anyone can share their first day experience at work with everyone.
             < p><strong>Technologies:</strong> React, Html, Css, Recaptcha</p>
             <div className="external-links">
             <a href="https://github.com/rahimestayfun/storepory" target="_blank" rel="noopener noreferrer"><Icon icon={githubIcon} width="45"/></a>
@@ -54,9 +60,11 @@ function Projects() {
         <hr/>
 
         <div className="project">
-          <img src={recipe} alt="recipe" className="project-image"/>
+          <div className="project-image">
+            <img src={recipe} alt="recipe" id="image"/>
+          </div>
           <div className="project-info">
-            <p>My recipe book is my mini project that helps you to keep your recipes. It was built with Html, Css and React with adding, showing, updating, and deleting recipe functionalities. All recipe data was stored in database.
+            <p><strong>My recipe book</strong> is my mini project that helps you to keep your recipes. It was built with Html, Css and React with adding, showing, updating, and deleting recipe functionalities. All recipe data was stored in database.
             <p><strong>Technologies:</strong> React, Postgresql, Html, Css</p>
             <div className="external-links">
             <a href="https://github.com/rahimestayfun/recipe_book" target="_blank" rel="noopener noreferrer"><Icon icon={githubIcon} width="45"/></a>
