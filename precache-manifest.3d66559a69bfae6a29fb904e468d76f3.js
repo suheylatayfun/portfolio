bc237b49774f21ed93ee1138c7e7294bb1929352
Rahime Tayfun <suheyladurna@gmail.com>
2020-04-16 11:35:56 -0500

    Updates

---
+++ precache-manifest.3d66559a69bfae6a29fb904e468d76f3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68d40337b05b269726f24af86c9bed60",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9e1210382f932830e421",
+    "url": "/portfolio/static/css/main.46b6a8ae.chunk.css"
+  },
+  {
+    "revision": "d1053c4fbd9471dcf808",
+    "url": "/portfolio/static/js/2.f81e486e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.f81e486e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e1210382f932830e421",
+    "url": "/portfolio/static/js/main.51e279b4.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  },
+  {
+    "revision": "97d24a3fff8a47a91e05c8a97aa12b61",
+    "url": "/portfolio/static/media/gift-away-1.97d24a3f.png"
+  },
+  {
+    "revision": "cd71fdc53169e9d318f0faf9d99c19ee",
+    "url": "/portfolio/static/media/recipe-book-1.cd71fdc5.png"
+  },
+  {
+    "revision": "6efe578d64749d1ae009ad6f183277b6",
+    "url": "/portfolio/static/media/storepo-1.6efe578d.png"
+  },
+  {
+    "revision": "248437b5f41b7a5bf1db0a61345f7d83",
+    "url": "/portfolio/static/media/volevent.248437b5.png"
+  }
+]);
