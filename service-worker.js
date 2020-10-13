3ccbc487564df4c24a200354f04c1339a36c57f3
Rahime Tayfun <suheyladurna@gmail.com>
2020-10-13 13:29:02 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.d56856c075c2ec9e19fb49353e2a9022.js"
+  "/portfolio/precache-manifest.bbddf8dc634bafb566d51b534792d222.js"
 );
 
 self.addEventListener('message', (event) => {
